7d0663aca35b4b1f4671773c66187662e87c9172
quangman96 <man.meo.69@gmail.com>
2020-05-20 20:56:56 +0700

    quangman

---
+++ src/main/java/com/ait/TkgtApplication.java
@@ -0,0 +1,13 @@
+package com.ait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkgtApplication.class, args);
+	}
+
+}
